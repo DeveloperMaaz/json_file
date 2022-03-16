04158ba447322cc80fd87d4c009a317c5652f03c
Maaz Ajmal <maazajmal564@gmail.com>
2022-03-16 09:25:50 +0500

    rc
    according to software changings

--- lib/features/presentation/model/model_data.dart
+++ lib/features/presentation/model/model_data.dart
@@ -1,13 +1,49 @@
-class ModelData {
-   String? name;
 
-   String? code;
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
 
-  ModelData(this.name, this.code);
+import 'dart:convert';
 
-  ModelData.fromJson(Map<String , dynamic>json){
+List<Welcome> welcomeFromJson(String str) => List<Welcome>.from(json.decode(str).map((x) => Welcome.fromJson(x)));
 
-    name = json['name'];
-    code = json['code'];
-  }
+String welcomeToJson(List<Welcome> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Welcome {
+    Welcome({
+        this.name,
+        this.code,
+    });
+
+    String? name;
+    String? code;
+
+    factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+        name: json["name"],
+        code: json["code"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "name": name,
+        "code": code,
+    };
 }
+
+
+///
+// class ModelData {
+//    String? name;
+//
+//    String? code;
+//
+//   ModelData(this.name, this.code);
+//
+//   ModelData.fromJson(Map<String , dynamic>json){
+//
+//     name = json['name'];
+//     code = json['code'];
+//   }
+// }
+
+
+
--- lib/features/presentation/widgets/json_screen.dart
+++ lib/features/presentation/widgets/json_screen.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:json_file/features/presentation/model/model_data.dart';
-import 'package:flutter/services.dart' as rootBundle;
+import 'package:flutter/services.dart';
 
 class JsonScreen extends StatefulWidget {
-  const JsonScreen({Key? key}) : super(key: key);
+  const JsonScreen({Key? key}) : super (key: key);
 
   @override
   _JsonScreenState createState() => _JsonScreenState();
@@ -16,40 +16,44 @@ class _JsonScreenState extends State<JsonScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Show Json Data'),),
-      body: FutureBuilder(
-        future: readJson(),
-        builder:(context,data){
-          if(data.hasError){
-            return Center(child: Text('${data.error}'),);
-          }
-          else if(data.hasData){
-            var ItemList= data.data as List<ModelData>;
-            return ListView.builder(itemBuilder: (context,index){
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  elevation: 4,
-                  child: ListTile(
-                    title: Text('${ItemList[index].name}'),
-                    subtitle: Text('${ItemList[index].code}'),
-                  ),
-                ),
-              );
-            });
-          }
-          else {
-            return const Center(child:  CircularProgressIndicator());
-          }
-        } ,
-
-      )
-      );
+        appBar: AppBar(title: const Text('Show Json Data'),),
+        body: FutureBuilder(
+            future: fetchData(context),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(child: Text('${snapshot.error}'),);
+              }
+              else if (snapshot.hasData) {
+                var ItemList = snapshot.data as List<Welcome>;
+                return ListView.builder(itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      elevation: 4,
+                      child: ListTile(
+                        title: Text('${ItemList[index].name}'),
+                        subtitle: Text('${ItemList[index].code}'),
+                      ),
+                    ),
+                  );
+                });
+              }
+              else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 
-   Future<List<ModelData>>readJson()async{
-   final jsonData=await rootBundle.rootBundle.loadString('jsons/json_data_model');
-   final jsonList= json.decode(jsonData) as List<dynamic>;
-   return jsonList.map((e) => ModelData.fromJson(e)).toList();
+  Future<List<Welcome>> fetchData(BuildContext context) async {
+    final jsonString = await DefaultAssetBundle.of(context).loadString(
+        'jsons/json_data_model');
+    return welcomeFromJson(jsonString);
+
   }
 }
+
+  /// first code without using software
+   // Future<List<ModelData>>readJson()async{
+   // final jsonData=await rootBundle.rootBundle.loadString('jsons/json_data_model');
+   // final jsonList= json.decode(jsonData) as List<dynamic>;
+   // return jsonList.map((e) => ModelData.fromJson(e)).toList();
